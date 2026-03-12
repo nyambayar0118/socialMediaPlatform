2d23827fbad5ca9cd28a6b55a0306cb7c1cac501
Zadyy <nyambayar2014@gmail.com>
2026-03-12 23:10:50 +0800

    Renamed user folder to users

---
+++ SocialMediaPlatform.Reddit.Core/Domain/Users/NormalUser.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+
+namespace SocialMediaPlatform.Reddit.Core.Domain.Users
+{
+    /// <summary>
+    /// Платформ дээрх энгийн хэрэглэгчийг төлөөлөх класс. Энэ нь платформ дээрх нийтлэг хэрэглэгчийн шинж чанаруудыг агуулна.
+    /// </summary>
+    public class NormalUser : User
+    {
+    }
+}
