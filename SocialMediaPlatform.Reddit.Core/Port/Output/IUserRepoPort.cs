ce4e2eab67b86e8abdddfe0b6bf6135211b6a4ef
Zadyy <nyambayar2014@gmail.com>
2026-03-12 21:12:05 +0800

    Implemented IUserRepoPort

--- SocialMediaPlatform.Reddit.Core/Port/Output/IUserRepoPort.cs
+++ SocialMediaPlatform.Reddit.Core/Port/Output/IUserRepoPort.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
-namespace SocialMediaPlatform.Core.Port.Output
+namespace SocialMediaPlatform.Reddit.Core.Ports.Output
 {
-    internal interface IUserRepoPort
+    /// <summary>
+    /// Хэрэглэгчийн репозиторийн Output Port интерфейс
+    /// </summary>
+    public interface IUserRepoPort
     {
+        /// <summary>Хэрэглэгч хадгалах</summary>
+        /// <param name="user">Хадгалах хэрэглэгчийн объект</param>
+        void Save(User user);
+
+        /// <summary>ID-аар хэрэглэгч хайх</summary>
+        /// <param name="userId">Хэрэглэгчийн ID дугаар</param>
+        /// <returns>Олдсон хэрэглэгчийн объект</returns>
+        User FindById(UserId userId);
+
+        /// <summary>Username-аар хэрэглэгч хайх</summary>
+        /// <param name="username">Хэрэглэгчийн нэр</param>
+        /// <returns>Олдсон хэрэглэгчийн объект</returns>
+        User FindByUsername(string username);
+
+        /// <summary>Хэрэглэгчийн мэдээлэл шинэчлэх</summary>
+        /// <param name="user">Шинэчлэх хэрэглэгчийн объект</param>
+        void Update(User user);
+
+        /// <summary>Хэрэглэгч устгах</summary>
+        /// <param name="userId">Устгах хэрэглэгчийн ID дугаар</param>
+        void Delete(UserId userId);
     }
 }
