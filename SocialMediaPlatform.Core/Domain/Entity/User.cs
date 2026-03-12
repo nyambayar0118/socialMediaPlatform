105a913c9d14a1c43867d09cfa76b6f53bb85719
Zadyy <nyambayar2014@gmail.com>
2026-03-12 20:01:35 +0800

    Implemented User

--- SocialMediaPlatform.Core/Domain/Entity/User.cs
+++ SocialMediaPlatform.Core/Domain/Entity/User.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Core.Domain.Entity
 {
-    internal class User
+    /// <summary>
+    /// Платформ дээрх хэрэглэгч класс
+    /// </summary>
+    public abstract class User : Entity
     {
+        /// <summary>
+        /// User-ийн ID дугаар
+        /// </summary>
+        public required UserId Id { get; init; }
+        /// <summary>
+        /// Хэрэглэгчийн username. Платформ дээр давхардсан username байхгүй байх ёстой
+        /// </summary>
+        public required string Username { get; init; }
+        /// <summary>
+        /// Хэрэглэгчийн email хаяг
+        /// </summary>
+        public required string Email { get; set; }
+        /// <summary>
+        /// Хэрэглэгчийн нууц үг
+        /// </summary>
+        public required string Password { get; set; }
     }
 }
