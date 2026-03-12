b49636b8461414ab99ce318d54f000925b104f27
Zadyy <nyambayar2014@gmail.com>
2026-03-12 23:10:32 +0800

    Renamed reaction folder to reactions

---
+++ SocialMediaPlatform.Reddit.Core/Domain/Reactions/Downvote.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+
+namespace SocialMediaPlatform.Reddit.Core.Domain.Reactions
+{
+    /// <summary>
+    /// Хэрэглэгчийн сэтгэл үл нийцсэнг илэрхийлэх Downvote класс.
+    /// </summary>
+    public class Downvote : Reaction
+    {
+    }
+}
---
+++ SocialMediaPlatform.Reddit.Core/Domain/Reactions/Upvote.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+
+namespace SocialMediaPlatform.Reddit.Core.Domain.Reactions
+{
+    /// <summary>
+    /// Хэрэглэгчийн сэтгэл нийцсэнг илэрхийлэх Upvote класс.
+    /// </summary>
+    public class Upvote : Reaction
+    {
+    }
+}
