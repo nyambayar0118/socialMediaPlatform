121f4e397bfde44c499166840dd0795505048f14
Zadyy <nyambayar2014@gmail.com>
2026-03-12 21:10:56 +0800

    Implemented IPostRepoPort

--- SocialMediaPlatform.Reddit.Core/Port/Output/IPostRepoPort.cs
+++ SocialMediaPlatform.Reddit.Core/Port/Output/IPostRepoPort.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
-namespace SocialMediaPlatform.Core.Port.Output
+namespace SocialMediaPlatform.Reddit.Core.Ports.Output
 {
-    internal interface IPostRepoPort
+    /// <summary>
+    /// Постын репозиторийн Output Port интерфейс
+    /// </summary>
+    public interface IPostRepoPort
     {
+        /// <summary>Пост хадгалах</summary>
+        /// <param name="post">Хадгалах постын объект</param>
+        void Save(Post post);
+
+        /// <summary>ID-аар пост хайх</summary>
+        /// <param name="postId">Постын ID дугаар</param>
+        /// <returns>Олдсон постын объект</returns>
+        Post FindById(PostId postId);
+
+        /// <summary>Нийт постын жагсаалт авах</summary>
+        /// <returns>Постын объектын жагсаалт</returns>
+        List<Post> FindAll();
+
+        /// <summary>Постын мэдээлэл шинэчлэх</summary>
+        /// <param name="post">Шинэчлэх постын объект</param>
+        void Update(Post post);
+
+        /// <summary>Пост устгах</summary>
+        /// <param name="postId">Устгах постын ID дугаар</param>
+        void Delete(PostId postId);
     }
 }
