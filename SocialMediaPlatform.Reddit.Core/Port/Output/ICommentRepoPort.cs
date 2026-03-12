10a31cba562dcfbf36ddfb4ebff9948ab46c4fd0
Zadyy <nyambayar2014@gmail.com>
2026-03-12 21:10:07 +0800

    Implemented ICommentRepoPort

--- SocialMediaPlatform.Reddit.Core/Port/Output/ICommentRepoPort.cs
+++ SocialMediaPlatform.Reddit.Core/Port/Output/ICommentRepoPort.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
-namespace SocialMediaPlatform.Core.Port.Output
+namespace SocialMediaPlatform.Reddit.Core.Ports.Output
 {
-    internal interface ICommentRepoPort
+    /// <summary>
+    /// Сэтгэгдлийн репозиторийн Output Port интерфейс
+    /// </summary>
+    public interface ICommentRepoPort
     {
+        /// <summary>Сэтгэгдэл хадгалах</summary>
+        /// <param name="comment">Хадгалах сэтгэгдлийн объект</param>
+        void Save(Comment comment);
+
+        /// <summary>ID-аар сэтгэгдэл хайх</summary>
+        /// <param name="commentId">Сэтгэгдлийн ID дугаар</param>
+        /// <returns>Олдсон сэтгэгдлийн объект</returns>
+        Comment FindById(CommentId commentId);
+
+        /// <summary>Постын сэтгэгдлүүдийг авах</summary>
+        /// <param name="postId">Постын ID дугаар</param>
+        /// <returns>Сэтгэгдлийн объектын жагсаалт</returns>
+        List<Comment> FindByPost(PostId postId);
+
+        /// <summary>Сэтгэгдэл устгах</summary>
+        /// <param name="commentId">Устгах сэтгэгдлийн ID дугаар</param>
+        void Delete(CommentId commentId);
     }
 }
