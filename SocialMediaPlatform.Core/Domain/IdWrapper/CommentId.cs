b711ad289ea3c472a82e1fa1ce44a90b762ec9d0
Zadyy <nyambayar2014@gmail.com>
2026-03-12 19:25:16 +0800

    Implemented CommentId

--- SocialMediaPlatform.Core/Domain/IdWrapper/CommentId.cs
+++ SocialMediaPlatform.Core/Domain/IdWrapper/CommentId.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace SocialMediaPlatform.Core.Domain.IdWrapper
 {
-    internal class CommentId
+    /// <summary>
+    /// Comment-ийн ID дугаарыг ялгахад зориулсан wrapper класс. Энэ нь Comment entity-ийн ID-г илэрхийлэх бөгөөд type safety-ийг хангахад тусална.
+    /// </summary>
+    public class CommentId
     {
+        /// <summary>
+        /// Comment-ийн ID дугаарын утга
+        /// </summary>
+        public required uint Value { get; init; }
+
+        public override string ToString() => Value.ToString();
     }
 }
