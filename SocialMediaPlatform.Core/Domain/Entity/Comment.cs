7f38c334e14a31efb1d818abb6e3c932eb4e5ab3
Zadyy <nyambayar2014@gmail.com>
2026-03-12 19:44:50 +0800

    Implemented comment

--- SocialMediaPlatform.Core/Domain/Entity/Comment.cs
+++ SocialMediaPlatform.Core/Domain/Entity/Comment.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Core.Domain.Entity
 {
-    internal class Comment
+    /// <summary>
+    /// Хэрэглэгчийн бичсэн сэтгэгдэл класс
+    /// </summary>
+    public abstract class Comment
     {
+        /// <summary>
+        /// Сэтгэгдлийн ID дугаар
+        /// </summary>
+        public required CommentId Id { get; init; }
+        /// <summary>
+        /// Сэтгэгдлийг бичсэн хэрэглэгчийн ID дугаар
+        /// </summary>
+        public required UserId AuthorId { get; init; }
+        /// <summary>
+        /// Сэтгэгдлийн агуулга
+        /// </summary>
+        public required string Content { get; init; }
     }
 }
