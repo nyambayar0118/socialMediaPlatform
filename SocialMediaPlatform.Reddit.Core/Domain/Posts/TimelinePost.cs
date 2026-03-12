515662e6a35e9ff2be79d73eedd4229c703ca3d6
Zadyy <nyambayar2014@gmail.com>
2026-03-12 23:10:10 +0800

    Renamed post folder to posts

---
+++ SocialMediaPlatform.Reddit.Core/Domain/Posts/TimelinePost.cs
@@ -0,0 +1,19 @@
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+
+namespace SocialMediaPlatform.Reddit.Core.Domain.Posts
+{
+    /// <summary>
+    /// Хэрэглэгчийн өөрийн Timeline дээрх пост класс
+    /// </summary>
+    public class TimelinePost : Post
+    {
+        /// <summary>
+        /// Постын гарчиг
+        /// </summary>
+        public required string Title { get; set; }
+        /// <summary>
+        /// Постын агуулга
+        /// </summary>
+        public required string Content { get; set; }
+    }
+}
