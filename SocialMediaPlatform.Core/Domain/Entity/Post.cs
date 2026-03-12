3b612f7e362ee989d9ba936b1f61f985628192f7
Zadyy <nyambayar2014@gmail.com>
2026-03-12 19:52:29 +0800

    Implemented Post

--- SocialMediaPlatform.Core/Domain/Entity/Post.cs
+++ SocialMediaPlatform.Core/Domain/Entity/Post.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using SocialMediaPlatform.Core.Domain.Enum;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SocialMediaPlatform.Core.Domain.Entity
 {
-    internal class Post
+    /// <summary>
+    /// Платформ дээр хэрэглэгчийн бичсэн пост класс
+    /// </summary>
+    public abstract class Post : Entity
     {
+        /// <summary>
+        /// Post-ийн ID дугаар
+        /// </summary>
+        public required PostId Id { get; init; }
+        /// <summary>
+        /// Post-ийг бичсэн хэрэглэгчийн ID дугаар
+        /// </summary>
+        public required UserId AuthorId { get; init; }
+        /// <summary>
+        /// Post-ийн харагдах байдал
+        /// </summary>
+        public VisibilityType Visibility { get; set; } = VisibilityType.Public;
     }
 }
