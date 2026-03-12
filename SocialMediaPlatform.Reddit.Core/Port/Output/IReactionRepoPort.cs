4ffc6e61cee00c0fb59b62cf16fffe3d47c4af91
Zadyy <nyambayar2014@gmail.com>
2026-03-12 21:11:17 +0800

    Implemented IReactionRepoPort

--- SocialMediaPlatform.Reddit.Core/Port/Output/IReactionRepoPort.cs
+++ SocialMediaPlatform.Reddit.Core/Port/Output/IReactionRepoPort.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Entity;
+using SocialMediaPlatform.Core.Domain.Enum;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
-namespace SocialMediaPlatform.Core.Port.Output
+namespace SocialMediaPlatform.Reddit.Core.Ports.Output
 {
-    internal interface IReactionRepoPort
+    /// <summary>
+    /// Reaction-ий репозиторийн Output Port интерфейс
+    /// </summary>
+    public interface IReactionRepoPort
     {
+        /// <summary>Reaction хадгалах</summary>
+        /// <param name="reaction">Хадгалах Reaction-ий объект</param>
+        void Save(Reaction reaction);
+
+        /// <summary>Reaction устгах</summary>
+        /// <param name="targetId">Объектын ID дугаар</param>
+        /// <param name="authorId">Reaction өгсөн хэрэглэгчийн ID дугаар</param>
+        void Delete(uint targetId, UserId authorId);
+
+        /// <summary>Объектын Reaction-ий тоог авах</summary>
+        /// <param name="targetId">Объектын ID дугаар</param>
+        /// <param name="targetType">Объектын төрөл</param>
+        /// <returns>Reaction-ий төрөл ба тоо</returns>
+        Dictionary<string, uint> CountByTarget(uint targetId, ReactionTargetType targetType);
+
+        /// <summary>Хэрэглэгч тухайн объектод Reaction өгсөн эсэхийг шалгах</summary>
+        /// <param name="userId">Хэрэглэгчийн ID дугаар</param>
+        /// <param name="targetId">Объектын ID дугаар</param>
+        /// <param name="targetType">Объектын төрөл</param>
+        /// <returns>Reaction өгсөн бол true, үгүй бол false</returns>
+        bool ExistsByUserAndTarget(UserId userId, uint targetId, ReactionTargetType targetType);
     }
 }
