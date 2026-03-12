5c773d5128e9b85d042984b512919371440c48e5
Zadyy <nyambayar2014@gmail.com>
2026-03-12 23:02:21 +0800

    Implemented PostFactory

--- SocialMediaPlatform.Reddit.Core/Factory/PostFactory.cs
+++ SocialMediaPlatform.Reddit.Core/Factory/PostFactory.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.IdWrapper;
+using SocialMediaPlatform.Reddit.Core.Domain.Posts;
 
 namespace SocialMediaPlatform.Reddit.Core.Factory
 {
-    internal class PostFactory
+    /// <summary>
+    /// Постын объект үүсгэх factory класс
+    /// </summary>
+    public class PostFactory
     {
+        /// <summary>Шинэ пост объект үүсгэх</summary>
+        /// <param name="id">Постын ID дугаар</param>
+        /// <param name="authorId">Постыг бичсэн хэрэглэгчийн ID дугаар</param>
+        /// <param name="title">Постын гарчиг</param>
+        /// <param name="content">Постын агуулга</param>
+        /// <returns>Үүсгэгдсэн постын объект</returns>
+        public TimelinePost Create(PostId id, UserId authorId, string title, string content)
+        {
+            return new TimelinePost
+            {
+                Id = id,
+                AuthorId = authorId,
+                Title = title,
+                Content = content,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
     }
 }
