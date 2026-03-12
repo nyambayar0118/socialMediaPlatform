9aa24c8721df7537a6af49081ddfe4e76f270fd2
Zadyy <nyambayar2014@gmail.com>
2026-03-12 21:04:19 +0800

    Implemented IReactionServicePort

--- SocialMediaPlatform.Reddit.Core/Port/Input/IReactionServicePort.cs
+++ SocialMediaPlatform.Reddit.Core/Port/Input/IReactionServicePort.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Enum;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
-namespace SocialMediaPlatform.Core.Port.Input
+namespace SocialMediaPlatform.Reddit.Core.Ports.Input
 {
-    internal interface IReactionServicePort
+    /// <summary>
+    /// Reaction үйлдлүүдийн Input Port интерфейс
+    /// </summary>
+    public interface IReactionServicePort
     {
+        /// <summary>Reaction нэмэх</summary>
+        /// <param name="targetId">Reaction өгөх объектын ID дугаар</param>
+        /// <param name="targetType">Reaction өгөх объектын төрөл</param>
+        /// <param name="userId">Reaction өгч буй хэрэглэгчийн ID дугаар</param>
+        /// <param name="reactionType">Reaction-ий төрөл</param>
+        void React(uint targetId, ReactionTargetType targetType, UserId userId, string reactionType);
+
+        /// <summary>Reaction цуцлах</summary>
+        /// <param name="targetId">Reaction цуцлах объектын ID дугаар</param>
+        /// <param name="targetType">Reaction цуцлах объектын төрөл</param>
+        /// <param name="userId">Reaction цуцлах хэрэглэгчийн ID дугаар</param>
+        void Unreact(uint targetId, ReactionTargetType targetType, UserId userId);
+
+        /// <summary>Reaction-ий тоог авах</summary>
+        /// <param name="targetId">Объектын ID дугаар</param>
+        /// <param name="targetType">Объектын төрөл</param>
+        /// <returns>Reaction-ий төрөл ба тоо</returns>
+        Dictionary<string, uint> GetReactionCount(uint targetId, ReactionTargetType targetType);
+
+        /// <summary>Хэрэглэгч Reaction өгсөн эсэхийг шалгах</summary>
+        /// <param name="targetId">Объектын ID дугаар</param>
+        /// <param name="targetType">Объектын төрөл</param>
+        /// <param name="userId">Хэрэглэгчийн ID дугаар</param>
+        /// <returns>Reaction өгсөн бол true, үгүй бол false</returns>
+        bool HasReacted(uint targetId, ReactionTargetType targetType, UserId userId);
     }
 }
