84803e9beafc3016f7cae71904a0c623308b5ccd
Zadyy <nyambayar2014@gmail.com>
2026-03-12 20:19:37 +0800

    Implemented PostDTO

--- SocialMediaPlatform.Core/Domain/DTO/PostDTO.cs
+++ SocialMediaPlatform.Core/Domain/DTO/PostDTO.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Enum;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Core.Domain.DTO
 {
-    internal class PostDTO
-    {
-    }
+    /// <summary>
+    /// Пост классын өгөгдлийн дамжуулах объект
+    /// </summary>
+    /// <param name="Id">Постын ID дугаар</param>
+    /// <param name="AuthorId">Постыг бичсэн хэрэглэгчийн ID дугаар</param>
+    /// <param name="Visibility">Постын харагдах байдал</param>
+    /// <param name="CreatedAt">Пост үүссэн огноо, цаг</param>
+    public record PostDTO(
+        PostId Id,
+        UserId AuthorId,
+        VisibilityType Visibility,
+        DateTime CreatedAt
+    );
 }
