b539e716c107da48aacaa44daecadebe47d7e69c
Zadyy <nyambayar2014@gmail.com>
2026-03-12 23:09:09 +0800

    Renamed comment folder to comments

---
+++ SocialMediaPlatform.Reddit.Core/Domain/Comments/MainComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocialMediaPlatform.Core.Domain.Entity;
+
+namespace SocialMediaPlatform.Reddit.Core.Domain.Comments
+{
+    /// <summary>
+    /// Reddit платформ дээрх Post-ний доорх MainComment класс
+    /// </summary>
+    public class MainComment : Comment
+    {
+    }
+}
