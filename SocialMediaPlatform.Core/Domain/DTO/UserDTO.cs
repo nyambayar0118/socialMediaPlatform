26264801b9520aebec57d09f45fffd48fdf24993
Zadyy <nyambayar2014@gmail.com>
2026-03-12 20:20:51 +0800

    Implemented UserDTO

--- SocialMediaPlatform.Core/Domain/DTO/UserDTO.cs
+++ SocialMediaPlatform.Core/Domain/DTO/UserDTO.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Core.Domain.DTO
 {
-    internal class UserDTO
-    {
-    }
+    /// <summary>
+    /// Хэрэглэгч классын өгөгдлийн дамжуулах объект
+    /// </summary>
+    /// <param name="Id">Хэрэглэгчийн ID дугаар</param>
+    /// <param name="Username">Хэрэглэгчийн нэр</param>
+    /// <param name="Email">Хэрэглэгчийн имэйл хаяг</param>
+    /// <param name="CreatedAt">Хэрэглэгч үүссэн огноо, цаг</param>
+    public record UserDTO(
+        UserId Id,
+        string Username,
+        string Email,
+        DateTime CreatedAt
+    );
 }
