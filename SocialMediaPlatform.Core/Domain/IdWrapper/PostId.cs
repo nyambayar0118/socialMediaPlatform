dc40a643083f4c706d7cb873695ee8f000f42360
Zadyy <nyambayar2014@gmail.com>
2026-03-12 19:27:30 +0800

    Implemented PostId

--- SocialMediaPlatform.Core/Domain/IdWrapper/PostId.cs
+++ SocialMediaPlatform.Core/Domain/IdWrapper/PostId.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace SocialMediaPlatform.Core.Domain.IdWrapper
 {
-    internal class PostId
+    /// <summary>
+    /// Post-ийн ID дугаарыг ялгахад зориулсан wrapper класс. Энэ нь Post entity-ийн ID-г илэрхийлэх бөгөөд type safety-ийг хангахад тусална.
+    /// </summary>
+    public class PostId
     {
+        /// <summary>
+        /// Post-ийн ID дугаарын утга
+        /// </summary>
+        public required uint Value { get; init; }
+
+        public override string ToString() => Value.ToString();
     }
 }
