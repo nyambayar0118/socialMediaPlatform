ecfaa1e52634c40bce6ac2c65d3c2f65c76cc40b
Zadyy <nyambayar2014@gmail.com>
2026-03-12 20:16:21 +0800

    Implemented CommentDTO

--- SocialMediaPlatform.Core/Domain/DTO/CommentDTO.cs
+++ SocialMediaPlatform.Core/Domain/DTO/CommentDTO.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Core.Domain.DTO
 {
-    internal class CommentDTO
-    {
-    }
+    /// <summary>
+    /// Сэтгэгдэл классын өгөгдлийн дамжуулах объект
+    /// </summary>
+    /// <param name="Id">Сэтгэгдлийн ID дугаар</param>
+    /// <param name="AuthorId">Сэтгэгдлийг бичсэн хэрэглэгчийн ID дугаар</param>
+    /// <param name="PostId">Харьяалагдах постын ID дугаар</param>
+    /// <param name="Content">Сэтгэгдлийн агуулга</param>
+    /// <param name="CreatedAt">Сэтгэгдэл үүссэн огноо, цаг</param>
+    public record CommentDTO(
+        CommentId Id,
+        UserId AuthorId,
+        PostId PostId,
+        string Content,
+        DateTime CreatedAt
+    );
 }
