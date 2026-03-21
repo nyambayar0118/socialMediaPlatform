7f49e0126b54ca8e6d481da087a04b566e8bd221
Zadyy <nyambayar2014@gmail.com>
2026-03-22 04:37:18 +0800

    Created Panels and Dialog files

---
+++ SocialMediaPlatform.Reddit.WinForms/Panels/LoginPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SocialMediaPlatform.Reddit.WinForms.Panels
+{
+    public partial class LoginPanel : UserControl
+    {
+        public LoginPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
