312a5792b6542a3b33dee2a51626d5713fa202cd
Zadyy <nyambayar2014@gmail.com>
2026-03-23 07:43:22 +0800

    Added login panel to mainform

--- SocialMediaPlatform.Reddit.WinForms/MainForm.Designer.cs
+++ SocialMediaPlatform.Reddit.WinForms/MainForm.Designer.cs
@@ -2,38 +2,37 @@
 {
     partial class MainForm
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private Panel contentPanel;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
-        }
+            contentPanel = new Panel();
+            SuspendLayout();
 
-        #endregion
+            // contentPanel
+            contentPanel.Dock = DockStyle.Fill;
+            contentPanel.Name = "contentPanel";
+
+            // MainForm
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1100, 680);
+            MinimumSize = new Size(800, 500);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Reddit Forums";
+
+            Controls.Add(contentPanel);
+
+            ResumeLayout(false);
+        }
     }
 }
--- SocialMediaPlatform.Reddit.WinForms/MainForm.cs
+++ SocialMediaPlatform.Reddit.WinForms/MainForm.cs
@@ -1,15 +1,37 @@
 using SocialMediaPlatform.Reddit.Core.Infrastructure;
+using SocialMediaPlatform.Reddit.WinForms.Panels;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
 
 namespace SocialMediaPlatform.Reddit.WinForms
 {
     public partial class MainForm : Form
     {
         private readonly Controller _controller;
+        private LoginPanel _loginPanel;
 
         public MainForm(Controller controller)
         {
             _controller = controller;
             InitializeComponent();
+            _loginPanel = new LoginPanel(_controller, this);
+            ShowLoginPanel();
         }
+
+        public void ShowLoginPanel()
+        {
+            contentPanel.Controls.Clear();
+            _loginPanel.Dock = DockStyle.Fill;
+            contentPanel.Controls.Add(_loginPanel);
+        }
+
+        public void ShowFeedPanel() { }    // implement later
+        public void ShowPostPanel(PostId postId) { }  // implement later
+        public void NavigateBackToFeed() { }   // implement later
+
+        public void ShowError(string message) =>
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        public void ShowInfo(string message) =>
+            MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
     }
 }
