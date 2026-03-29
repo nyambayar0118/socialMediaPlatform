549f599b8180ef7ab79f123715af6a4d83ffcf7e
Zadyy <nyambayar2014@gmail.com>
2026-03-29 17:41:46 +0800

    Created result record to be used in the main logic

---
+++ ProfilePictureProcessor/ProcessResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfilePictureProcessor
+{
+    public record ProcessResult
+    {
+        public bool Success { get; init; }
+
+        public string ErrorMessage { get; init; }
+
+        public string ResultPath { get; init; }
+    }
+}
