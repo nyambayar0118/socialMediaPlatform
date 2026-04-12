2c29c067f11a6b25d32a1819cc62367938b4a0de
Zadyy <nyambayar2014@gmail.com>
2026-04-13 04:21:15 +0800

    Implemented Comment adapter and mapper

--- SocialMediaPlatform.Reddit.Infrastructure/Persistence/Sqlite/Mappers/CommentMapper.cs
+++ SocialMediaPlatform.Reddit.Infrastructure/Persistence/Sqlite/Mappers/CommentMapper.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Data.Sqlite;
+using SocialMediaPlatform.Core.Domain.IdWrapper;
+using SocialMediaPlatform.Reddit.Core.Domain.Comments;
+using SocialMediaPlatform.Core.Domain.Entity;
 
 namespace SocialMediaPlatform.Reddit.Infrastructure.Persistence.Sqlite.Mappers
 {
-    internal class CommentMapper
+    /// <summary>
+    /// Сэтгэгдлийг SQLite мөр болон domain объект хооронд хөрвүүлэх Mapper
+    /// </summary>
+    public static class CommentMapper
     {
+        /// <summary>SQLite мөрийг Maincomment объект болгоно</summary>
+        /// <param name="reader">SQLite өгөгдөл уншигч объект</param>
+        public static Comment ToDomain(SqliteDataReader reader)
+        {
+            return new MainComment
+            {
+                Id = new CommentId { Value = (uint)reader.GetInt64(0) },
+                PostId = new PostId { Value = (uint)reader.GetInt64(1) },
+                AuthorId = new UserId { Value = (uint)reader.GetInt64(2) },
+                Content = reader.GetString(3),
+                CreatedAt = DateTime.Parse(reader.GetString(4))
+            };
+        }
     }
 }
